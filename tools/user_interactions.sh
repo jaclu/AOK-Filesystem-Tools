e8c0159f6deab14cdea285674f139e4af00fadc6
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2025-01-09 11:44:38 +0100

    New setting IOS_MOUNTS

--- tools/user_interactions.sh
+++ tools/user_interactions.sh
@@ -16,37 +16,15 @@
 #  tools resources to be available!
 #
 
-thisfs_is_iCloud_mounted() {
-    mount | grep -wq iCloud
+this_fs_is_mounted() {
+    mount | grep -wq "$1"
 }
 
-#===============================================================
-#
-#   User interactions during deploy
-#
-#===============================================================
+should_it_be_mounted() {
+    d_mount_path="$1"
+    msg_2 "should_it_be_mounted($d_mount_path)"
 
-user_interactions() {
-    msg_2 "user_interactions()"
-
-    should_icloud_be_mounted
-
-    if [ -z "$AOK_TIMEZONE" ]; then
-        msg_1 "Timezone selection"
-        /opt/AOK/common_AOK/usr_local_bin/set-timezone
-    fi
-    # msg_3 "user_interactions()  - done"
-}
-
-should_icloud_be_mounted() {
-    msg_2 "should_icloud_be_mounted()"
-
-    if ! this_is_ish; then
-        msg_3 "This is not iSH, skipping /iCloud mount check"
-        return
-    fi
-
-    if thisfs_is_iCloud_mounted; then
+    if thisfs_is_mounted "$d_mount_path"; then
         msg_3 "was already mounted, returning"
         return
     fi
@@ -72,17 +50,17 @@ should_icloud_be_mounted() {
         unset sibm_dependency
     fi
 
-    _sibm_text="Do you want to mount /iCloud now?"
+    _sibm_text="Do you want to mount $d_mount_path now?"
     # --topleft \
     "$_sibm_dlg_app" \
-        --title "Mount /iCloud" \
+        --title "Mount $d_mount_path" \
         --yesno "$_sibm_text" 0 0
 
     _sibm_exitstatus=$?
 
     if [ "$_sibm_exitstatus" -eq 0 ]; then
-        mount -t ios x /iCloud
-        msg_1 "/iCloud has been mounted!"
+        mount -t ios x "$d_mount_path"
+        msg_1 "$d_mount_path has been mounted!"
     else
         msg_3 "mount rejected"
     fi
@@ -90,7 +68,35 @@ should_icloud_be_mounted() {
     unset _sibm_dlg_app
     unset _sibm_text
     unset _sibm_exitstatus
-    # msg_3 "should_icloud_be_mounted()  done"
+    # msg_3 "should_it_be_mounted()  done"
+}
+
+check_defined_mounts() {
+    msg_2 "check_defined_mounts($IOS_MOUNTS)"
+
+    if ! this_is_ish; then
+        msg_3 "This is not iSH, skipping iOS mount check"
+        return
+    fi
+
+    # shellcheck disable=SC2086 # this should be expanded
+    set -- $IOS_MOUNTS
+    for d_mount_path in "$@"; do
+	this_fs_is_mounted "$d_mount_path" && continue
+	should_it_be_mounted "$d_mount_path"
+    done
+}
+
+user_interactions() {
+    msg_2 "user_interactions()"
+
+    check_defined_mounts
+
+    if [ -z "$AOK_TIMEZONE" ]; then
+        msg_1 "Timezone selection"
+        /opt/AOK/common_AOK/usr_local_bin/set-timezone
+    fi
+    # msg_3 "user_interactions()  - done"
 }
 
 #===============================================================
